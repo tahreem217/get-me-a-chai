eabcad14832084ba2b70160adf57d90ac9b81c7e
tahreem217 <tahreemfatima21766@gmail.com>
2026-03-10 20:25:35 +0530

    fix: ensure db connection in Username server component

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,34 +1,34 @@
- 
 import React from 'react'
 import PaymentPage from '../../components/PaymentPage'
 import { notFound } from 'next/navigation'
 import connectDb from "@/lib/dbConnect"
 import User from '@/models/Users'
-const Username =async ({params}) => {
-  const checkUser=async()=>{
-    let u=await User.findOne({username:params.username});
-  if(!u){
+
+const Username = async ({ params }) => {
+ 
+  await connectDb();
+
+ 
+  const { username } = params;
+
+ 
+  const u = await User.findOne({ username: username });
+  
+  if (!u) {
     return notFound();
   }
-  }
-  await checkUser();
- 
-  
-  if(!params.username)
-    {
-     return notFound();
-    }
+
   return (
     <>
-  
-    <PaymentPage params={params}/>
+      <PaymentPage params={params}/>
     </>
   )
 }
 
-export default  Username
-export async function generateMetadata({params}){
+export default Username
+
+export async function generateMetadata({ params }){
   return {
-    title: `${params.username}-Get me a chai`,
-}
+    title: `${params.username} - Get me a chai`,
+  }
 }
